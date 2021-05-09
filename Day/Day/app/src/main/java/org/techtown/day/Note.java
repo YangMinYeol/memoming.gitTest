f30f7092dbc1d6d6015ac547701a2e710e7960c8
YangMinYeol <sheep0125@naver.com>
2021-05-09 22:50:53 +0900

    Day 1

---
+++ Day/Day/app/src/main/java/org/techtown/day/Note.java
@@ -0,0 +1,49 @@
+package org.techtown.day;
+
+public class Note {
+    String contents;
+    String Dday;
+    String createDate;
+    String pircture;
+    String mood;
+
+    public String getContents() {
+        return contents;
+    }
+
+    public void setContents(String contents) {
+        this.contents = contents;
+    }
+
+    public String getDday() {
+        return Dday;
+    }
+
+    public void setDday(String dday) {
+        Dday = dday;
+    }
+
+    public String getCreateDate() {
+        return createDate;
+    }
+
+    public void setCreateDate(String createDate) {
+        this.createDate = createDate;
+    }
+
+    public String getPircture() {
+        return pircture;
+    }
+
+    public void setPircture(String pircture) {
+        this.pircture = pircture;
+    }
+
+    public String getMood() {
+        return mood;
+    }
+
+    public void setMood(String mood) {
+        this.mood = mood;
+    }
+}
